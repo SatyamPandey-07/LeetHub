54eca01241ad622102f24a99b664210498a762e8
Satyam Pandey <satyam53@tsecol.onmicrosoft.com>
2026-07-02 17:42:22 +0530

    Time: 3 ms (64.63%), Space: 11.9 MB (99.78%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -6,7 +6,6 @@ public:
         for(int i=1; i<n; i++){
             bool same=s[i]==s[i-1];
             prev=(-same & prev)+(-!same & cur);
-            //cur=(-same & (cur+1))+(-!same & 1);
             cur=1+(-same & cur );
             cnt+=cur<=prev;
         }
